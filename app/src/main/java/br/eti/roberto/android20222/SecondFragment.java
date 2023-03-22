a4c97f366c7a0a9a86bf643b321d89b543f404e0
Roberto Franco <roberto.yuri.franco@gmail.com>
2023-03-22 19:44:52 -0300

    Camera

--- app/src/main/java/br/eti/roberto/android20222/SecondFragment.java
+++ app/src/main/java/br/eti/roberto/android20222/SecondFragment.java
@@ -1,9 +1,14 @@
 package br.eti.roberto.android20222;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -14,6 +19,27 @@ import br.eti.roberto.android20222.databinding.FragmentSecondBinding;
 public class SecondFragment extends Fragment {
 
     private FragmentSecondBinding binding;
+    private ImageView imageView;
+    private Button button;
+    static final int REQUEST_IMAGE_CAPTURE = 1710;
+
+    private void dispatchTakePicture(){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null){
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == getActivity().RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView = getActivity().findViewById(R.id.imageView);
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
 
     @Override
     public View onCreateView(
@@ -29,6 +55,13 @@ public class SecondFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        button = getActivity().findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePicture();
+            }
+        });
         binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
