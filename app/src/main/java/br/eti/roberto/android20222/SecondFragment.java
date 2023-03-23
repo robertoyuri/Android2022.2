70b836d1261d210e2b7c1482676a304d816c2df4
Roberto Franco <roberto.yuri.franco@gmail.com>
2023-03-22 21:11:17 -0300

    Location

--- app/src/main/java/br/eti/roberto/android20222/SecondFragment.java
+++ app/src/main/java/br/eti/roberto/android20222/SecondFragment.java
@@ -1,18 +1,29 @@
 package br.eti.roberto.android20222;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.location.Location;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import br.eti.roberto.android20222.databinding.FragmentSecondBinding;
 
@@ -22,18 +33,19 @@ public class SecondFragment extends Fragment {
     private ImageView imageView;
     private Button button;
     static final int REQUEST_IMAGE_CAPTURE = 1710;
+    private FusedLocationProviderClient fusedLocationClient;
 
-    private void dispatchTakePicture(){
+    private void dispatchTakePicture() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if(takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null){
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
     }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data){
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == getActivity().RESULT_OK){
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == getActivity().RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             imageView = getActivity().findViewById(R.id.imageView);
@@ -46,6 +58,26 @@ public class SecondFragment extends Fragment {
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this.getContext());
+        if (ActivityCompat.checkSelfPermission(this.getActivity(),
+                Manifest.permission.ACCESS_COARSE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this.getActivity(),
+                        Manifest.permission.ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED) {
+
+        }
+            fusedLocationClient.getLastLocation().addOnSuccessListener(getActivity(),
+                    new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            if(location != null){
+                                Log.d("location", location.toString());
+                                Toast.makeText(getContext(), location.toString(), Toast.LENGTH_LONG).show();
+                            }
+
+                        }
+                    });
 
         binding = FragmentSecondBinding.inflate(inflater, container, false);
         return binding.getRoot();
